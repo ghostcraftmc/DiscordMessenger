804f863b59d73500d1be47984579803a6661881f
ProtoGhost <67905221+ProtoGhost@users.noreply.github.com>
2022-02-06 01:20:32 +0530

    added revokePunishment

--- src/main/java/com/pepedevs/discordmessenger/listener/EventListener.java
+++ src/main/java/com/pepedevs/discordmessenger/listener/EventListener.java
@@ -5,6 +5,7 @@ import com.Zrips.CMI.events.CMIPlayerVanishEvent;
 import com.pepedevs.discordmessenger.DiscordMessenger;
 import com.pepedevs.discordmessenger.messagable.DiscordEmbed;
 import me.leoko.advancedban.bukkit.event.PunishmentEvent;
+import me.leoko.advancedban.bukkit.event.RevokePunishmentEvent;
 import me.leoko.advancedban.utils.Punishment;
 import org.bukkit.GameMode;
 import org.bukkit.event.EventHandler;
@@ -71,5 +72,21 @@ public class EventListener implements Listener {
 
     }
 
+    @EventHandler
+    public void revokePunishment (RevokePunishmentEvent event) {
+        Punishment punish = event.getPunishment();
+        DiscordEmbed msg = DiscordEmbed.builder()
+                .color(Color.decode("#32FB7D"))
+                .author(new DiscordEmbed.EmbedAuthor("PUNISHMENT REMOVED","https://cdn.discordapp.com/attachments/836464907984568330/939115383024140338/green-hook-mark-0ef5edf305f7b7af6ea1ddd9ec7aef54.png",null))
+                .title(new DiscordEmbed.EmbedTitle( "Survival S9",null))
+                .field(new DiscordEmbed.EmbedField("Name",punish.getName(),true))
+                .field(new DiscordEmbed.EmbedField("ID",String.valueOf(punish.getId()),true))
+                .field(new DiscordEmbed.EmbedField("Punished By",punish.getOperator(),true))
+                .field(new DiscordEmbed.EmbedField("Type",punish.getType().getName(),true))
+                .field(new DiscordEmbed.EmbedField("Reason",punish.getReason(),true))
+                .timestamp(Instant.now())
+                .build();
+        DiscordMessenger.sendMessage("939091234872508436",msg);
+    }
 
 }
